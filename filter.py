e4bd498154854d3d5761970f372f4be531dd4968
afrinaldi <107689282+afrinaldipdg@users.noreply.github.com>
2025-06-08 17:31:37 +0700

    Delete filter.py

--- filter.py
+++
@@ -1,7 +0,0 @@
-import requests
-try:
-    response = requests.get("https://www.google.com", timeout=10)
-    response.raise_for_status()
-    print("Google.com accessed successfully.")
-except requests.RequestException as e:
-    print(f"Failed to access Google.com: {e}")
